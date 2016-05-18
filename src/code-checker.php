40089cdcd081455ba56bfb3a4c6150309182484c
Martin Zlámal <mrtnzlml@gmail.com>
2016-05-18 17:12:52 +0200

    fixup! Indentation: allow inline comments

--- src/code-checker.php
+++ src/code-checker.php
@@ -362,7 +362,7 @@ $checker->tasks[] = function (CodeChecker $checker, $s) {
 			$line = $m[0][1] ? substr_count($orig, "\n", 0, $m[0][1]) + 1 : 1;
 			$checker->error('Mixed tabs and spaces indentation', $line);
 		}
-		if (preg_match('#(?<=[^/])(?<=[\S ])\t#', $s, $m, PREG_OFFSET_CAPTURE)) {
+		if (preg_match('#(?<=[^/\#])(?<=[\S ])\t#', $s, $m, PREG_OFFSET_CAPTURE)) {
 			$line = substr_count($orig, "\n", 0, $m[0][1]) + 1;
 			$checker->error('Found unexpected tabulator', $line);
 		}
